878bc5339c6f2aa592ac24d131c9a62fa7bd1910
Sweta Baid <Sweta.Baid.ext@flydubai.com>
2019-02-18 11:53:37 +0530

    dashboard indicator changes

--- src/app/dashboard-indicator/dashboard-indicator.component.ts
+++ src/app/dashboard-indicator/dashboard-indicator.component.ts
@@ -9,14 +9,10 @@ import { AmChartsService, AmChart } from "@amcharts/amcharts3-angular";
 })
 export class DashboardIndicatorComponent implements OnInit {
   private chart:AmChart;
-  constructor(private AmCharts:AmChartsService) { }
+  constructor(private amCharts:AmChartsService) { }
 
   ngOnInit() {
-  
-  setInterval(this.GetCounter, 1000);
-  }
-  ngAfterViewInit() {
-    this.chart = this.AmCharts.makeChart("chartdiv", {
+    this.chart = this.amCharts.makeChart("chartdiv", {
       "theme": "none",
       "type": "gauge",
       "axes": [{
@@ -65,10 +61,40 @@ export class DashboardIndicatorComponent implements OnInit {
           }]
   });
   
+ 
   }
+  ngAfterViewInit() {
+    setInterval(updateChart => {
+      this.amCharts.updateChart(this.chart, () => {
+        // Change whatever properties you want
+        this.chart.dataProvider = [];
+        var now = new Date().getTime();
+        var countDownDate = new Date("Feb 14, 2019 11:37:25").getTime();
+        // Find the distance between now and the count down date
+        var distance = countDownDate - now;
+        // Time calculations for days, hours, minutes and seconds
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        // Output the result in an element with id="demo"
+        var displayTime = hours + ":"
+            + minutes + ":" + seconds;
+        // If the count down is over, write some text 
+        if (distance < 0) {
+           var displayMsg = "Time Expired";
+            return;
+        }
+        this.chart.arrows[0].setValue(hours);
+        this.chart.axes[0].setTopText(displayTime);
+        this.chart.axes[0].bands[1].setEndValue(hours);
+      });
+
+    }, 1000);
+  }
+  
   ngOnDestroy() {
     if (this.chart) {
-      this.AmCharts.destroyChart(this.chart);
+      this.amCharts.destroyChart(this.chart);
     }
   }
 // set random value
